ec2eb9c1bdce44bb2f9e36f0131fcb03e0d62271
Sanaullah Ansari <asanaullah129@gmail.com>
2024-08-31 23:26:40 +0530

    Completed project plan and schema of the project, added generate token in the user service

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from "express";
-import { createUser, isUserAvailableCheck } from "../services/UserService";
+import { createUser, generateToken, isUserAvailableCheck } from "../services/UserService";
 import bcrypt from 'bcrypt';
 import { errorLogService } from "../services/CommonService";
-import jwt from 'jsonwebtoken';
 
 export const registerUser = async (req: Request, res: Response) => {
     try {
@@ -12,15 +11,15 @@ export const registerUser = async (req: Request, res: Response) => {
         }
 
         const existingUser = await isUserAvailableCheck(username, emailId);
-        
+
         if (existingUser) {
             return res.status(404).json({ statusId: 3, status: "User already exists with current username or emailId" });
-        } 
-        
+        }
+
         const hashedPassword: string = await bcrypt.hash(password, 10);
-        
+
         const newUser = await createUser(username, emailId, hashedPassword);
-        
+
         if (newUser) {
             return res.status(200).json({ statusId: 1, status: "User Created Successfully" });
         }
@@ -53,18 +52,9 @@ export const loginUser = async (req: Request, res: Response) => {
         if (existingUser && isPasswordCorrect) {
             const jwtSecret = process.env.JWT_TOKEN as string;
 
-            if (jwtSecret) {
-                const token: string = jwt.sign(
-                    {
-                        username: existingUser.username,
-                        password: existingUser.password,
-                        id: existingUser.id,
-                    },
-                    jwtSecret,
-                    { expiresIn: "1d" }
-                );
-                return res.status(200).json({ token: token });
-            }
+            const token = await generateToken(jwtSecret, existingUser.username, existingUser.password, existingUser.id);
+
+            return res.status(200).json({ token: token });
 
         }
         else {
--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from "@prisma/client";
+import jwt from 'jsonwebtoken';
 
 const prisma = new PrismaClient();
 
 export const isUserAvailableCheck = async (username: string, emailId: string) => {
-    const user = await prisma.userDetials.findFirst({
+    const user = await prisma.userDetails.findFirst({
         where: {
             OR: [
                 { username: username },
@@ -16,11 +17,29 @@ export const isUserAvailableCheck = async (username: string, emailId: string) =>
 };
 
 export const createUser = async (username: string, emailId: string, password: string) => {
-    const newUser = await prisma.userDetials.create({
+    const newUser = await prisma.userDetails.create({
         data: {
             username: username, emailId: emailId, password: password
         }
     });
-    
+
     return newUser;
+}
+
+export const generateToken = async (jwtSecret: string, username: string, password: string, id: string): Promise<string> => {
+
+    var token: string = "";
+
+    if (jwtSecret) {
+        token = jwt.sign(
+            {
+                username: username,
+                password: password,
+                id: id,
+            },
+            jwtSecret,
+            { expiresIn: "1d" }
+        );
+    }
+    return token;
 }
